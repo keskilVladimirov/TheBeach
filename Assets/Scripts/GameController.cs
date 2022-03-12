5fd270c8f918ad47993f56faa7e5181855c612a2
keskil <k.vladimirov@social-infinite.com>
2022-03-13 03:01:59 +0900

    fix

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,12 +5,10 @@ public class GameController : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private ObjectPooler objectPooler;
     
-
     public void OnPointerClick(PointerEventData eventData)
     {
         var name = eventData.pointerCurrentRaycast.gameObject.name;
         var position = eventData.pointerCurrentRaycast.screenPosition;
-
         objectPooler.SpawnFromPool(name, new Vector3(position.x, position.y, 0), Quaternion.identity);
     }
 }
